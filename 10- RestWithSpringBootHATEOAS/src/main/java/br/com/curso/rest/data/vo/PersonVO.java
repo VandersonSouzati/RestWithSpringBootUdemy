92619abd7c8d7d0627bdfde641ebc83c19cfdb6e
Vanderson <vandersonsouzati@gmail.com>
2020-05-31 16:44:48 -0300

    Implementando Hateoas

--- 10- RestWithSpringBootHATEOAS/src/main/java/br/com/curso/rest/data/vo/PersonVO.java
+++ 10- RestWithSpringBootHATEOAS/src/main/java/br/com/curso/rest/data/vo/PersonVO.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.github.dozermapper.core.Mapping;
 
+@JsonPropertyOrder({"id","firstName", "lastName", "address", "gender", "operation"})
 public class PersonVO extends ResourceSupport implements Serializable {
 	private static final long serialVersionUID = 1L;
 
